6c4515f8b53a79bd3afb9d70334bd6ad55e15396
usmysa <usmysa@gmail.com>
2024-07-18 22:54:19 +0900

    add alias

--- apps/api/next.config.mjs
+++ apps/api/next.config.mjs
@@ -1,4 +1,15 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname),
+    };
+
+    return;
+  },
+};
 
 export default nextConfig;
